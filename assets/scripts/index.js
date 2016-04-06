6b6644d51fc4f9fca8799a61bb611f686a70e7a5
bryansarnold <bryansarnold@gmail.com>
2016-04-06 17:36:10 -0400

    fixed all linter errors mostly in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const winning = require ('./winning.js');
-
 // const index = require('./bundle.js');
 
 // user require with a reference to bundle the file and use it in this file
@@ -11,53 +9,76 @@ const winning = require ('./winning.js');
 // require('./example');
 
 
-      let setMessage = function(msg) {
-        document.getElementById("message").innerText = msg;
-      };
+const setMessage = function(msg) {
+  document.getElementById("message").innerText = msg;
+  // $("#message").text(msg)
+};
 
-      let startGame = function(setMessage) {
-        document.turn = "X";
-        document.winner = null;
-        setMessage(document.turn + " gets to start first!");
-      };
+const startGame = function(setMessage) {
+  document.turn = "X";
+  document.winner = null;
+  setMessage(document.turn + " gets to start first!");
+};
 
-      let switchTurn = function() {
-          if (winningMove(document.turn)) {
-            setMessage("Congrats " + document.turn + " wins it!");
-            document.winner = document.turn;
-        } else if (document.turn === "O") {
-          setMessage("It's " + document.turn + "'s turn!");
-          document.turn = "O";
+const winningMove = function(move) {
+  let result;
+  let theWinner;
+  if (theWinner(1, 2, 3, move) ||
+      theWinner(4, 5, 6, move) ||
+      theWinner(7, 8, 9, move) ||
+      theWinner(1, 4, 7, move) ||
+      theWinner(2, 5, 8, move) ||
+      theWinner(3, 6, 9, move) ||
+      theWinner(1, 5, 9, move) ||
+      theWinner(3, 5, 7, move)) {
+        result = true;
+    }
+    return result;
+  };
 
-        } else {
-          document.turn = "X";
-          setMessage("It's " + document.turn + "'s turn!");
-        }
-      };
+// }
 
-      let nextTurn = function(box) {
-        if (document.winner !== null) {
-          setMessage(document.winner + " already won!");
-        } else if (box.innerText === "") {
-            box.innerText = document.turn;
-          switchTurn();
-        } else {
-          setMessage("Box is Taken");
-        }
-      };
+const nextTurn = function(box) {
+  let switchTurn;
+  if (document.winner !== null) {
+    setMessage(document.winner + " already won!");
+  } else if (box.innerText === "") {
+      box.innerText = document.turn;
+    switchTurn();
+  } else {
+    setMessage("Box is Taken");
+  }
+};
 
-      let theWinner = function (a, b, c, move) {
-        let result = false;
-        if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
-          result = true;
-        }
-        return result;
-      };
+const switchTurn = function() {
+    if (winningMove(document.turn)) {
+      setMessage("Congrats " + document.turn + " wins it!");
+      document.winner = document.turn;
+  } else if (document.turn === "O") {
+    setMessage("It's " + document.turn + "'s turn!");
+    document.turn = "O";
 
-      let getBox = function(num) {
-        return document.getElementById("box" + num).innerText;
-      };
+  } else {
+    document.turn = "X";
+    setMessage("It's " + document.turn + "'s turn!");
+  }
+};
 
+const getBox = function(num) {
+  return document.getElementById("box" + num).innerText;
+};
+
+const theWinner = function (a, b, c, move) {
+  let result = false;
+  if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
+    result = true;
+  }
+  return result;
+};
+
+$(document).ready(function(){
+  //code here
+});
 
 module.exports = {
   setMessage,
