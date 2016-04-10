9eb173e96734e44d7d13c2024803d6bcf0f1feca
bryansarnold <bryansarnold@gmail.com>
2016-04-10 00:21:04 -0400

    added game api file

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -13,37 +13,38 @@ const signUp = (success, failure, data) => {
 
 const signIn = (success, failure, data) => {
   $.ajax({
-    method: 'POST',
-    url: app.api + '/sign-in',
-    data,
-    // ? use the back slash or not on sign-in
-  }).done(success)
-  .fail(failure);
+      method: 'POST',
+      url: app.api + '/sign-in',
+      data,
+      // ? use the back slash or not on sign-in
+    }).done(success)
+    .fail(failure);
 };
 
 const signOut = (success, failure) => {
   console.log("Sign out confirmed");
   $.ajax({
-    method: 'DELETE',
-    url: app.api + '/sign-out/' + app.user.id,
-    headers: {
-      Authorization: 'Token token=' + app.user.token,
-    },
-    //why place a comma after curly braces
-  }).done(success)
-  .fail(failure);
+      method: 'DELETE',
+      url: app.api + '/sign-out/' + app.user.id,
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      },
+      //why place a comma after curly braces
+    }).done(success)
+    .fail(failure);
 };
 
 const changePassword = (success, failure, data) => {
   $.ajax({
-    method: 'PATCH',
-    url: app.api + '/change-password/' + app.user.id, data,
-    headers: {
-      Authorization: 'Token token=' + app.user.token,
-      // ? what is up with the token-token thing
-    }
-  }).done(success)
-  .fail(failure);
+      method: 'PATCH',
+      url: app.api + '/change-password/' + app.user.id,
+      data,
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+        // ? what is up with the token-token thing
+      }
+    }).done(success)
+    .fail(failure);
 };
 
 
